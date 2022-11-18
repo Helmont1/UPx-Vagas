492696ff8858482dfacd3e58a8d810552fa848b2
Jean <jeanluccadias@gmail.com>
2022-11-18 12:44:00 -0300

    Update

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,26 +1,20 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ScreenA } from "../Screens/ScreenA";
 import { ScreenB } from "../Screens/ScreenB";
 import { SpotDetail } from "../Screens/SpotDetail";
 import Scanner from "../Screens/Scanner";
+import Login from "../Screens/Login";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
 export function StackRoutes() {
 
-  // curl -X 'GET' \
-  // 'https://upx4api2022.azurewebsites.net/spot' \
-  // -H 'accept: application/json'  
-
-  const [parkingSpots, setParkingSpots] = React.useState([]);
-
-
-  
-
 
   return (
     <Navigator>
+      <Screen name="Login" component={Login} />
       <Screen name="Home" component={ScreenA} />
       <Screen name="Vagas" component={ScreenB}  />
       <Screen name="spotDetail" component={SpotDetail} />
